ffe63cb60d04a52fff16d48f0f63ae5c89756bf5
Obolduy <ivanox01@yandex.ru>
2025-10-12 20:08:03 +0300

    Drop division match rand in generateCalculationValue

--- src/Games/BrainCalc/logic.php
+++ src/Games/BrainCalc/logic.php
@@ -7,7 +7,7 @@ function generateCalculationValue(): array
     $firstNumber = rand(1, 10);
     $secondNumber = rand(1, 10);
 
-    $expression = match (rand(1, 4)) {
+    $expression = match (rand(1, 3)) {
         1 => ['expression_text' => "$firstNumber + $secondNumber", 'expression_answer' => $firstNumber + $secondNumber],
         2 => ['expression_text' => "$firstNumber - $secondNumber", 'expression_answer' => $firstNumber - $secondNumber],
         3 => ['expression_text' => "$firstNumber * $secondNumber", 'expression_answer' => $firstNumber * $secondNumber],
